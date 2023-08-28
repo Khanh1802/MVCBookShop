e460a5d7a93c79fd44537f83a75400c20748495e
Phuong Khanh <Khanh1802@github.com>
2023-08-28 08:59:33 +0700

    Create DbContext and add migrations database Categori

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookShopWeb.Models
 {
+    [Table("Category")]
     public class Category
     {
         [Key]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
