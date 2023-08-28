8ac8a514caf9c6263dbea4090ba808d315293499
Phuong Khanh <Khanh1802@github.com>
2023-08-28 13:20:20 +0700

    Create table Category and update bottstrap.css

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BookShopWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
