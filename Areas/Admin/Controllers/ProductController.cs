2365ca5e0d478c90a7e5afc482c3ca390810fa3f
Phuong Khanh <Khanh1802@github.com>
2023-09-02 23:49:00 +0700

    CRUD Product

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,24 +1,84 @@
-﻿using BookShopWeb.DataAccess.Data;
+﻿using BookShopWeb.DataAccess.Repositories;
+using BookShopWeb.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace BookShopWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IProductRepository _productRepository;
 
-        public ProductController(ApplicationDbContext context)
+        public ProductController(IProductRepository productRepository)
         {
-            _context = context;
+            _productRepository = productRepository;
         }
 
         public async Task<IActionResult> Index()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _productRepository.GetAllAsync();
             return View(products);
         }
 
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.Add(product);
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+        public async Task<IActionResult> Update(Guid id)
+        {
+            if (id == null || id == Guid.Empty)
+            {
+                return NotFound();
+            }
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.Update(product);
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            if (id == null || id == Guid.Empty)
+            {
+                return NotFound();
+            }
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Product product)
+        {
+            _productRepository.Delete(product);
+            return RedirectToAction("Index", "Product");
+        }
     }
 }
